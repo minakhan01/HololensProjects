71b1cb1c943059c549e2c67c31cf448a17e67f72
Mina Khan <Mina Khan>
2017-03-24 15:39:36 -0700

    vectrocity and arrow

--- Assets/GestureInput.cs
+++ Assets/GestureInput.cs
@@ -4,30 +4,47 @@ using UnityEngine;
 
 public class GestureInput : MonoBehaviour {
 
-    private Vector3 position;
+    private Vector3 arrowOrigin;
+    private Vector3 arrowTarget;
     public GameObject line;
-    public GameObject ball;
+    public GameObject ball;//big ball
+    public GameObject smallBall;
     private bool finishedUpdate;
     private Vector3 forceVector;
 
     // Use this for initialization
     void Start() {
         finishedUpdate = false;
+        arrowOrigin = smallBall.transform.position;
     }
 
-    // Update is called once per frame
     void Update() {
-        LineRenderer lineRenderer= line.GetComponent<LineRenderer>();
-        lineRenderer.SetPosition(1, position*3);
-        AnimationCurve curve = new AnimationCurve();
-        curve.AddKey(0.0f, 0.0f);
-        curve.AddKey(1.0f, 1.0f);
-        lineRenderer.widthCurve = curve;
-        lineRenderer.widthMultiplier = 1.0f;
+
+    }
+
+    void drawArrow(Vector3 change)
+    {
+        arrowOrigin = smallBall.transform.position;
+        arrowTarget = arrowOrigin + change;
+
+        LineRenderer lineRenderer = line.GetComponent<LineRenderer>();
+        AnimationCurve arrowShapedCurve = new AnimationCurve(new Keyframe(0, 0.4f),
+            new Keyframe(0.9f, 0.4f),
+            new Keyframe(0.91f, 1f),
+            new Keyframe(1, 0f)
+        );
+        lineRenderer.widthCurve = arrowShapedCurve;
+        Vector3[] arrowpositions = new Vector3[] {
+            arrowOrigin,
+            Vector3.Lerp(arrowOrigin,arrowTarget,0.9f),
+            Vector3.Lerp(arrowOrigin,arrowTarget,0.91f),
+            arrowTarget };
+        lineRenderer.numPositions = arrowpositions.Length;
+        lineRenderer.SetPositions(arrowpositions);
     }
 
     public void updatePosition(Vector3 newPosition) {
-        position = newPosition;
+        drawArrow(newPosition);
     }
 
     private void FixedUpdate()
@@ -41,10 +58,9 @@ public class GestureInput : MonoBehaviour {
 
     public void updateFinished(Vector3 newPosition)
     {
-        position = newPosition;
+        //arrowTarget = newPosition;
         ball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         finishedUpdate = true;
-        forceVector = new Vector3(position.x * 100, position.y * 100, 100 * Mathf.Abs(position.z));
-        //forceVector = new Vector3(0, 0, 10);
+        forceVector = new Vector3(arrowTarget.x * 100, arrowTarget.y * 100, 100 * Mathf.Abs(arrowTarget.z));
     }
 }
